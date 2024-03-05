13ce0da4ca3b971be9a306e880c1c0d38e7de739
Eugene <eugene.lot77@gmail.com>
2024-03-05 14:59:26 +0200

    Added pagination

--- comments/views.py
+++ comments/views.py
@@ -1,12 +1,24 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from comments.models import Comment
-from comments.serializers import CommentSerializer, CommentListSerializer, CommentDetailSerializer
+from comments.serializers import (
+    CommentSerializer,
+    CommentListSerializer,
+    CommentDetailSerializer,
+)
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 25
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         queryset = self.queryset
