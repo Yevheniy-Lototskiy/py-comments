82981f5564ea2d91ebaa3d31d84b5e2f1333ba99
Eugene <eugene.lot77@gmail.com>
2024-03-05 15:03:00 +0200

    Implemented filters into views, refreshed requirements

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
 
 from comments.models import Comment
@@ -19,12 +20,31 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     pagination_class = CustomPagination
+    filter_backends = [DjangoFilterBackend]
+    filter_set_fields = ["username", "email", "pub_date"]
 
     def get_queryset(self):
         queryset = self.queryset
 
         if self.action == "list":
             queryset = queryset.filter(parent_comment__isnull=True)
+
+            # Filter by fields
+            sort_by = self.request.query_params.get("sort_by")
+            if sort_by:
+                queryset = queryset.order_by(sort_by)
+
+        # Filter by values
+        username = self.request.query_params.get('username', None)
+        email = self.request.query_params.get('email', None)
+        pub_date = self.request.query_params.get('pub_date', None)
+
+        if username:
+            queryset = queryset.filter(username=username)
+        if email:
+            queryset = queryset.filter(email=email)
+        if pub_date:
+            queryset = queryset.filter(pub_date=pub_date)
 
         return queryset
 
