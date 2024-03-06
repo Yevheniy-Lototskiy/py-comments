60681ea57d01b2a559c2732b88564d7c7f4cd378
Eugene <eugene.lot77@gmail.com>
2024-03-06 21:31:15 +0200

    Added pagination with filtering objects

--- comments/views.py
+++ comments/views.py
@@ -11,61 +11,40 @@ from comments.serializers import (
 )
 
 
-# class CustomPagination(PageNumberPagination):
-#     page_size = 25
-#     page_size_query_param = 'page_size'
-#     max_page_size = 100
-
-
-# class CommentViewSet(viewsets.ModelViewSet):
-#     queryset = Comment.objects.all()
-#     serializer_class = CommentSerializer
-#     pagination_class = CustomPagination
-#     filter_backends = [DjangoFilterBackend]
-#     filter_set_fields = ["username", "email", "pub_date"]
-#
-#     def get_queryset(self):
-#         queryset = self.queryset
-#
-#         if self.action == "list":
-#             queryset = queryset.filter(parent_comment__isnull=True)
-#
-#             # Filter by fields
-#             sort_by = self.request.query_params.get("sort_by")
-#             if sort_by:
-#                 queryset = queryset.order_by(sort_by)
-#
-#         # Filter by values
-#         username = self.request.query_params.get('username', None)
-#         email = self.request.query_params.get('email', None)
-#         pub_date = self.request.query_params.get('pub_date', None)
-#
-#         if username:
-#             queryset = queryset.filter(username=username)
-#         if email:
-#             queryset = queryset.filter(email=email)
-#         if pub_date:
-#             queryset = queryset.filter(pub_date=pub_date)
-#
-#         return queryset
-#
-#     def get_serializer_class(self):
-#         if self.action == "list":
-#             return CommentListSerializer
-#
-#         if self.action == "update":
-#             return CommentDetailSerializer
-#
-#         return CommentSerializer
+class CustomPagination(PageNumberPagination):
+    page_size = 25
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class CommentListView(APIView):
+    pagination_class = CustomPagination
+
     def get(self, request):
         # Filtering by parent_comment_id can be removed,
         # it is applied for cascading display in json format
         comments = Comment.objects.all().filter(parent_comment_id__isnull=True)
-        serializer = CommentListSerializer(comments, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # Filtering objects by fields
+        sort_by = request.query_params.get("sort_by")
+        if sort_by:
+            comments = comments.order_by(sort_by)
+
+        # Filtering objects by values
+        username = request.query_params.get('username')
+        if username:
+            comments = comments.filter(username=username)
+        email = request.query_params.get("email")
+        if email:
+            comments = comments.filter(email=email)
+        pub_date = request.query_params.get("pub_date")
+        if pub_date:
+            comments = comments.filter(pub_date=pub_date)
+
+        paginator = self.pagination_class()
+        result_page = paginator.paginate_queryset(comments, request)
+        serializer = CommentListSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self, request):
         serializer = CommentCreateSerializer(data=request.data)
@@ -92,4 +71,3 @@ class CommentDetailView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
